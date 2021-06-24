880e397f55007b7dede48fda4df18ece5f7a877c
MohdSaif <MohdSaif@nextgen.com>
2021-06-24 20:20:23 +0530

    double slash

--- src/Api/index.js
+++ src/Api/index.js
@@ -1,7 +1,7 @@
 import Axios from 'axios';
 
 // const url = 'http://localhost:5000';
-const url = 'https://immense-dusk-33096.herokuapp.com/';
+const url = 'https://immense-dusk-33096.herokuapp.com';
 
 // customer
 export const addCustomerApi = (data) => Axios.post(`${url}/api/v1/customer/add`, data);
