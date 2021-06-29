0c272d9560a66c7937e2b85fc00a0d92396956c5
MohdSaif <MohdSaif@nextgen.com>
2021-06-30 00:07:04 +0530

    APi

--- src/Api/index.js
+++ src/Api/index.js
@@ -1,8 +1,8 @@
 import Axios from 'axios';
 
-const url = 'http://localhost:5000';
+// const url = 'http://localhost:5000';
 // const url = 'https://cabletvinternetserver.herokuapp.com';
-// const url = 'https://cabeltvinternetserver.herokuapp.com';
+const url = 'https://cabeltvinternetserver.herokuapp.com';
 
 // customer
 export const addCustomerApi = (data) => Axios.post(`${url}/v1/customer/add`, data);
